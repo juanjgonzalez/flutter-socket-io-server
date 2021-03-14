41581aee1917bc718f635bc2e2c8b5bd9e31a45b
juan jose gonzalezs <juansgj.91@gmail.com>
2021-03-14 11:49:50 +0100

    cambios sobre bandas ter

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,24 @@
 
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+const dateTime = new Date();
+
+console.log('init server: ' + dateTime);
+console.log('-----------------------');
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon jovi'));
+bands.addBand(new Band('ACDC'));
+bands.addBand(new Band('Rolling stones'));
+//console.log(bands);
 //Mensajes de sockets
 io.on('connection', client =>{
 
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands() );
 
     client.on('disconnect', () => {
         console.log('Cliente Desconectado');
@@ -12,6 +27,22 @@ io.on('connection', client =>{
     client.on('mensaje', (payload)=>{
         console.log('Mensaje!!', payload);
         io.emit('mensaje', {admin:'Nuevo mensaje'});
+    });
+
+    client.on('vote-band', (payload) =>{
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) =>{
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) =>{
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 
 });
